8d5fcbefc12c2568d3092706868c6f58212fc236
Joshua Niederer <josh@jpniederer.com>
2017-08-03 21:55:53 -0500

    Minor updates; cleaned up routing, filter shelf contents.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import Book from './Book';
 import Search from './Search';
 import Shelf from './Shelf';
@@ -14,7 +14,6 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: true,
     allBooks: []
   }
 
@@ -27,50 +26,26 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <div>
-          <Route path="/search" render={() => (
-            <Search />
-          )} />
-        </div>
-
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-                
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+        <Route exact path="/" render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
               <div>
-                <Shelf title="Currently Reading" books={this.state.allBooks} />
-                <Shelf title="Want to Read" books={this.state.allBooks} />
-                <Shelf title="Read" books={this.state.allBooks} />
+                <Shelf title="Currently Reading" shelfName="currentlyReading" books={this.state.allBooks} />
+                <Shelf title="Want to Read" shelfName="wantToRead" books={this.state.allBooks} />
+                <Shelf title="Read" shelfName="read" books={this.state.allBooks} />
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search">Add a Book</Link>
             </div>
           </div>
-        )}
+        )} />
+        <Route path="/search" render={() => (
+          <Search />
+        )} />
       </div>
     )
   }
--- src/Shelf.js
+++ src/Shelf.js
@@ -4,19 +4,28 @@ import Book from './Book'
 import './App.css';
 
 class Shelf extends Component {
+  state = {
+    booksOnShelf: []
+  }
+  
   static propTypes = {
     title: PropTypes.string.isRequired,
-    //books: PropTypes.object.isRequired,
+    books: PropTypes.array.isRequired,
+  }
+
+  componentDidMount() {
+
   }
 
   render() {
+    let booksOnShelf = this.props.books.filter((book) => this.props.shelfName === book.shelf);
+
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            
-            {this.props.books.map((book) => (
+            {booksOnShelf.map((book) => (
               <li key={book.id} className="">
                 <Book {...book}  />
               </li>
